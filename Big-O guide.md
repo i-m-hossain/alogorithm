525cad559b4d9a7b9cc67b5287c93b0f62a1dd36
mdimran1409036 <mdimran1409036@gmail.com>
2023-05-01 07:51:14 +0600

    big o guide added

---
+++ Big-O guide.md
@@ -0,0 +1,4 @@
+1. Calculation not dependent on input size - O(1)
+2. Single loop - O(n)
+3. Nested loops - O(n^2)
+4. Input size reduced by half - O(logn)
--- sorting/insertionSort.js
+++ sorting/insertionSort.js
@@ -3,13 +3,26 @@
 //* const arr=[-6, 20, 8, -2,4]
 //* insertionSort(arr) => should return [-6, -2, 4, 8, 20]
 
-/*
+/* Algorithm
  * Virtually split the array into a sorted and an unsorted part
  * Assume that the first element is already sorted and remaining elements are unsorted
  * select an unsorted element and compare with all elements in the sorted part
  * if the elements in the sorted parent is smaller than the selected element, proceed to the next element in the unsorted part. Else, shift larger elements in the sorted part towards the right.
  * insert the selected element at the right index
  */
+
+/* NTI(Number to insert) SE(Sorted element)
+    [-6 20 8 -2 4]  NTI = 20    -6 > 20 ? No.    place 20 to the right of -6
+    [-6 20 8 -2 4]  NTI = 8     20 > 8 ? Yes.    shift 20 to the right
+    [-6 20 20 -2 4] NTI = 8     -6 > 8 ? No.     place 8 to the right of -6
+    [-6 8 20 -2 4]  NTI = -2    20 > -2 ? Yes.   shift 20 to the right
+    [-6 8 20 20 4]  NTI = -2    8  > -2 ? yes    shift 8 to the right
+    [-6 8 8 20 4]   NTI = -2    -6 > -2 ? No     place -2 to the right of -6
+    [-6 -2 8 20 4]  NTI = 4     20 > 4 ? Yes     shift 20 to the right
+    [-6 -2 8 20 20] NTI = 4     8  > 4 ? Yes     shift 8 to the right
+    [-6 -2 8 8 20]  NTI = 4    -2  > 4 ? No      place 4 to the right of -2
+    [-6 -2 4 8 20]  Reached the end of the array, insertion sort finished         
+*/ 
 
 function insertionSort(arr) {
     for (let i = 1; i <= arr.length - 1; i++) {
@@ -25,7 +38,7 @@ function insertionSort(arr) {
 
         let j = i - 1;
         while (j >= 0 && arr[j] > NTI) {
-            arr[j + 1] = arr[j];
+            arr[j+1] = arr[j];
             j--;
         }
         arr[j + 1] = NTI;
@@ -34,3 +47,6 @@ function insertionSort(arr) {
 }
 
 console.log(insertionSort([-6, 20, 8, -2, 4]));
+
+
+//? Big O: O(n^2)
